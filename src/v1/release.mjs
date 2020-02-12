9df0ab6972e1e3be7fef494d23bc3120a0387037
jascha <jascha@jaeh.at>
2020-02-12 20:50:58 +0100

    v1/release: almost there...

--- src/v1/release.mjs
+++ src/v1/release.mjs
@@ -1,13 +1,16 @@
-import crypto from '@webboot/crypto'
+import path from 'path'
 
+import fs from '@magic/fs'
 import is from '@magic/types'
+
+import crypto from '@webboot/crypto'
 
 const libName = '@webboot/api/v1/release'
 
 export const release = async (req, res) => {
   const headers = { 'Content-Type': 'text/plain' }
 
-  if (req.method !== 'POST' || is.empty(req.body)) {
+  if (req.method !== 'POST') {
     const code = 405
     const body = `${libName}: Method not allowed. use POST`
 
@@ -18,28 +21,65 @@ export const release = async (req, res) => {
     }
   }
 
-  const { version, key, fingerprint } = req.body.signed
+  const { sig, signed } = req.body
 
-  console.log(req.body.signed)
+  const queueDir = path.join(process.cwd(), 'queue')
 
-  // const importKey = await crypto.gpg.import(key)
+  const fullPath = path.join(queueDir, signed.domain, signed.version)
 
-  // if (!importKey) {
-    // const code = 500
-    // const body = `${libName}: internal error, could not import gpg key`
-    // return {
-      // body,
-      // code,
-      // headers
-    // }
-  // }
+  const exists = await fs.exists(fullPath)
 
-  // const code = 200
-  // const body = 'received request. started computations.'
+  if (exists) {
+    const code = 403
+    const body = `${libName}: This version of the homepage is in the queue already.`
 
-  // return {
-    // body,
-    // code,
-    // headers,
-  // }
+    return {
+      body,
+      code,
+      headers,
+    }
+  }
+
+  await fs.mkdirp(path.dirname(fullPath))
+
+  const jsonPath = `${fullPath}.json`
+  await fs.writeFile(jsonPath, JSON.stringify(signed))
+
+  const sigPath = `${fullPath}.sig`
+  await fs.writeFile(sigPath, sig)
+
+  const importKey = await crypto.gpg.import(signed.key)
+
+  if (!importKey) {
+    const code = 500
+    const body = `${libName}: internal error, could not import gpg key`
+    return {
+      body,
+      code,
+      headers
+    }
+  }
+
+  const verified = await crypto.gpg.verify({ sig: sigPath, file: jsonPath })
+
+  if (!verified) {
+    const code = 403
+    const body = `${libName}: signature could not be verified`
+
+    return {
+      body,
+      code,
+      headers,
+    }
+  }
+
+  const code = 200
+  const href = 'https://builds.webboot.org/'
+  const body = `received request add added it to the queues. to see the status visit: ${href}`
+
+  return {
+    body,
+    code,
+    headers,
+  }
 }
