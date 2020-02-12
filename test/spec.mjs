3a72e8b7126c6d5a953b9062f20a72a25e1d9263
jascha <jascha@jaeh.at>
2020-02-12 20:51:39 +0100

    rename publish to release

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,9 @@
 import { is } from '@magic/test'
 
-import { publish, key, fingerprint } from '../src/v1/index.mjs'
+import { release, key, fingerprint } from '../src/v1/index.mjs'
 
 export default [
-  { fn: () => publish, expect: is.function },
+  { fn: () => release, expect: is.function },
   { fn: () => key, expect: is.function },
   { fn: () => fingerprint, expect: is.function },
 ]
